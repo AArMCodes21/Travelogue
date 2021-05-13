6d52eea03b693d4798e224f662dcc0fc92f925c9
AArMcode <aarmcodesinit21@gmail.com>
2021-05-13 10:33:23 +0530

    Included git ignore, make sure to download node modeules

--- Travelogue-main/README.md
+++
@@ -1,3 +0,0 @@
-# Travelogue
-A simple travel website, which manges your whole trip.
-Created HTML, CSS files for home page starters, and a folder name assets containing images and styles.css file.
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
